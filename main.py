8817789d6756ebbf5430a96b587621a0368c852a
Anna <a.karlysheva@ukma.edu.ua>
2023-06-14 14:33:57 +0200

    base bot

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+import telebot
+
+BOT_TOKEN = '6249367774:AAFOlNFVbE71oxVEnN-sE7B5b0V9zLAOj4Y'
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+if __name__ == '__main__':
+    print('PyCharm')
+
