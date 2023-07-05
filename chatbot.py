41d28dcb23b4824cd51b53209e2eb255bd3ccf49
erisa <erisa.hoxha@ffuf.de>
2023-07-05 16:54:20 +0200

    chatbot conversation

---
+++ chatbot.py
@@ -0,0 +1,95 @@
+import re
+import random
+# import long_responses as long
+
+R_EATING = "I don't like eating anything because I'm a bot obviously!"
+R_ADVICE = "If I were you, I would go to the internet and type exactly what you wrote there!"
+
+
+def unknown():
+    response = ["Could you please re-phrase that? ",
+                "...",
+                "Sounds about right.",
+                "What does that mean?"][
+        random.randrange(4)]
+    return response
+
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certainty = 0
+    has_required_words = True
+
+    # Counts how many words are present in each predefined message
+    # Counts how many words from the user are in the predefined message
+    for word in user_message:
+        if word in recognised_words:
+            message_certainty += 1
+
+    # Calculates the percent of recognised words in a user message
+    percentage = float(message_certainty) / float(len(recognised_words))
+
+    # Checks that the required words are in the string
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
+
+    # Must either have the required words, or be a single response
+    if has_required_words or single_response:
+        return int(percentage * 100)
+    else:
+        return 0
+
+
+def check_all_messages(message):
+    highest_prob_list = {}
+
+    # Simplifies response creation / adds it to the dict
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+
+    # Responses -------------------------------------------------------------------------------------------------------
+    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
+    response('See you!', ['bye', 'goodbye'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+    response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
+    response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
+    response('What symptoms do you have?', ['feeling', 'well', 'not', 'good', 'been'], required_words=['not', 'feeling'])
+    response('And what is your age?', ['feeling_nervous', 'panic', 'breathing_rapidly', 'sweating', 'trouble_in_concentration', 'having_trouble_in_sleeping', 'having_trouble_with_work', 'hopelessness', 'anger', 'over_react', 'change_in_eating', 'suicidal_thought', 
+        'feeling_tired',
+        'close_friend',
+        'social_media_addiction',
+        'weight_gain',
+        'introvert',
+        'popping_up_stressful_memory',
+        'having_nightmares',
+        'avoids_people_or_activities',
+        'feeling_negative',
+        'trouble_concentrating',
+        'blamming_yourself',
+        'hallucinations',
+        'repetitive_behaviour',
+        'seasonally',
+        'increased_energy'])
+
+    # Longer responses
+    response(R_ADVICE, ['give', 'advice'], required_words=['advice'])
+    response(R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
+
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+    # print(highest_prob_list)
+    # print(f'Best match = {best_match} | Score: {highest_prob_list[best_match]}')
+
+    return unknown() if highest_prob_list[best_match] < 1 else best_match
+
+
+# Used to get the response
+def get_response(user_input):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    response = check_all_messages(split_message)
+    return response
+
+
+# Testing the response system
+while True:
+    print('Bot: ' + get_response(input('You: ')))
