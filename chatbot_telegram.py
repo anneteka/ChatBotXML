28f6f581252599c2e66365cf8a5c1c95ddd678f4
Anna <a.karlysheva@ukma.edu.ua>
2023-07-12 22:45:58 +0200

    chatbot disclaimer message

--- chatbot_telegram.py
+++ chatbot_telegram.py
@@ -8,7 +8,15 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Disclaimer: this bot is not a valid diagnostic instrument, however, you can use it to "
+                          "evaluate your symptom and get useful information. "
+                          "\n To start testing process use /testing command.")
+
+
+@bot.message_handler(commands=['testing'])
+def start_testing(message):
+    print(message)
+    bot.send_message(chat_id=message.chat.id, text="What symptoms do you currently experience?")
 
 
 @bot.message_handler(func=lambda msg: True)
