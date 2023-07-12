7aea64ca373834f9d5f7f95ec8311d190f59000a
Anna <a.karlysheva@ukma.edu.ua>
2023-07-12 12:36:03 +0200

    get question for a symptom

--- process_message.py
+++ process_message.py
@@ -3,15 +3,13 @@ from lxml import etree as ET
 
 def process_answer(text):
     tree = ET.parse('resources/answers.xml')
-    root = ET.Element("root")
-    text = str(text).lower()
+    text = str(text).lower().strip()
     res = tree.xpath(f"//KeyWords[@text='{text}']/../@answer")[0]
     return res
 
 
 def process_symptoms(text):
     tree = ET.parse('resources/symptoms.xml')
-    root = ET.Element("root")
     text = str(text).lower().replace(" and ", ",")
     texts = text.split(",")
     res = []
@@ -20,9 +18,15 @@ def process_symptoms(text):
         s = tree.xpath(f"//KeyWords[@text='{symptom}']/../@name")
         if s is not None and s != []:
             res.append(s[0])
+    return res
 
+
+def get_question(symptom):
+    tree = ET.parse('resources/symptoms.xml')
+    res = tree.xpath(f"//Symptom[@name='{symptom}']/Question/@text")[0]
     return res
 
 
 print(process_answer("a lot"))
 print(process_symptoms("tired and sad,"))
+print(get_question("hallucinations"))
