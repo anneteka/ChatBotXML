e196fa8621b6f200a900a0229907d260b2ae8067
valentina09876 <valentinamurati25@gmail.com>
2023-07-20 23:31:40 +0200

    apply xpath function string()

--- prepare/xml_processing/process_xml.py
+++ prepare/xml_processing/process_xml.py
@@ -30,13 +30,13 @@ def process_symptoms(text):
 
 def get_question(symptom):
     tree = ET.parse('../../resources/data/symptoms.xml')
-    res = tree.xpath(f"//*[@name='{symptom}']/Question/@text")[0]
+    res = tree.xpath(f"string(//*[@name='{symptom}']/Question/@text)")
     return res
 
 
 def node_id_data(xml_text, node_id):
     tree = ET.fromstring(xml_text)
-    name = tree.xpath(f"//*[@id='{node_id}']/@name")[0]
+    name = tree.xpath(f"string(//*[@id='{node_id}']/@name)")
     node_type = tree.xpath(f"name(//*[@id='{node_id}'])")
     return node_type, name
 
@@ -44,15 +44,15 @@ def node_id_data(xml_text, node_id):
 def process_current_node(xml_text, node_id):
     tree = ET.fromstring(xml_text)
 
-    child_name = tree.xpath(f"//*[@id='{node_id}']/*/@name")[0]
+    child_name = tree.xpath(f"string(//*[@id='{node_id}']/*/@name)")
     child_node_type = tree.xpath(f"name(//*[@id='{node_id}']/*)")
-    child_id = tree.xpath(f"//*[@id='{node_id}']/*/@id")[0]
+    child_id = tree.xpath(f"string(//*[@id='{node_id}']/*/@id)")
     return child_node_type, child_name, child_id
 
 
 def get_next_node(xml_text, node_id, answer):
     tree = ET.fromstring(xml_text)
-    new_id = tree.xpath(f"//*[@id='{node_id}']/*[@answer='{answer}']/@id")[0]
+    new_id = tree.xpath(f"string(//*[@id='{node_id}']/*[@answer='{answer}']/@id)")
     return new_id
 
 # print(process_answer("a lot"))
